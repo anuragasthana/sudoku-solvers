f5f0ac0f3f757e43e2e673d4144426758de82d24
anuragasthana <anurag.a.asthana@gmail.com>
2024-03-11 14:16:49 -0700

    adding part of generate

---
+++ data_generate.py
@@ -0,0 +1,22 @@
+from sudoku import Sudoku
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import os
+from tqdm import tqdm as progress_bar
+
+def check_data():
+    if not os.file.exists('data.npz'):
+        generate()
+    # put in dataloader to send to main
+    
+
+def generate():
+    inputs = []
+    labels = []
+    for _, _ in progress_bar(enumerate(range(100000)), total=100000):
+        puzzle = Sudoku(3).difficulty(np.random.uniform(.25, .75))
+        inputs.append(np.array(puzzle.board))
+        solution = puzzle.solve()
+        labels.append(np.array(solution.board))
+    np.savez('data.npz', inputs=inputs, labels=labels)
+    print("Data saved")
